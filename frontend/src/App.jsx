14f5c6bfa5f04353d4c9bb9d76daa7aac197f6ce
KayIssy54 <mwongerajoy4@gmail.com>
2026-07-03 12:58:41 +0300

    Add react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Inventory from "./components/Inventory";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Inventory Management System</h1>
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
